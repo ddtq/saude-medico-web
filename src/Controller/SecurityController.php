1e85f9abcd3ba9e753c2a9ff6b7595e9209f492c
RenanBrs <renan.barbosards@gmail.com>
2020-04-14 13:34:45 -0300

    Concluido logout do Admin, com autenticação

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
- 
+use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
 
 class SecurityController extends AbstractController
 {
@@ -32,8 +34,12 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout()
+    public function logout(Request $request, Response $response, TokenInterface $token)
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        foreach ($this->cookies as $cookieName => $cookieData) {
+            $response->headers->clearCookie($cookieName, $cookieData['path'], $cookieData['domain'], isset($cookieData['secure']) ? $cookieData['secure'] : false, true, isset($cookieData['samesite']) ? $cookieData['samesite'] : null);
+        }
+        return $this->redirectToRoute('/admin/login');
+        // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
