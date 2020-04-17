03ffd7ce15fb304833c63c73720844f1d35a1b95
RenanBrs <renan.barbosards@gmail.com>
2020-04-17 12:55:15 -0300

    add try catch e role permission

--- src/Controller/ManagementMedicoController.php
+++ src/Controller/ManagementMedicoController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Provider\UserAuthenticationProvider;
 use Symfony\Component\Security\Core\User\UserChecker;
@@ -10,13 +11,23 @@ use Symfony\Component\Security\Core\User\UserChecker;
 class ManagementMedicoController extends AbstractController
 {
     /**
-     * @Route("/medico/management/medico", name="management_medico")
+     * @Route("/medico/management", name="management_medico")
      */
     public function index()
     {
-        
-        return $this->render('management_medico/index.html.twig', [
-            'controller_name' => 'ManagementMedicoController',   
-        ]);
+        try {
+            //code...
+            // // or add an optional message - seen by developers
+            $this->denyAccessUnlessGranted('ROLE_USER_MEDICO', null, 'User tried to access a page without having ROLE_ADMIN');
+            $this->denyAccessUnlessGranted('ROLE_USER_MEDICO');
+ 
+            return $this->render('management_medico/index.html.twig', [
+                'controller_name' => 'ManagementMedicoController',   
+            ]);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return new Response($th);
+        }
+
     }
 }
